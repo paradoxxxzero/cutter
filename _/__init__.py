c016bd9d8b5e345129a2a16826dfe604ed128d13
Florian Mounier <florian.mounier@kozea.fr>
2012-04-20 18:39:57 +0200

    Python 2: Here we go ellipsis

--- _/__init__.py
+++ _/__init__.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint
 
+
+class EllipsisGetter(object):
+    def __getitem__(self, key):
+        return key
+
+ellipsis = EllipsisGetter()[...]
+
+
 is_string = lambda s: (
     isinstance(s, bytes) or
     isinstance(s, str))
@@ -19,25 +27,35 @@ def flatten(iterable):
     ] if is_iterable(iterable) else [iterable]
 
 
-def cut(iterable, *args):
+def _cut(iterable, *args):
     """Cut a list by index or arg"""
     void = object()
     if not args:
         args = 0,
 
     if len(args) > 1:
-        iterable = cut(iterable, *args[:-1])
+        iterable = _cut(iterable, *args[:-1])
     index = args[-1]
-    if index == ...:
+    if index == ellipsis:
         return flatten(iterable)
 
     def cut_item(item):
-        if isinstance(item, list) and index.isdigit():
-            return item[int(index)] if len(item) > int(index) else void
+        if isinstance(item, list) and isinstance(index, int):
+            return item[index] if len(item) > index else void
         if isinstance(item, dict):
-            return item.get(index, getattr(item, index, void))
-        return getattr(item, index, void)
+            return item.get(index, getattr(item, str(index), void))
+        return getattr(item, str(index), void)
     return list(filter(lambda x: x is not void, map(cut_item, iterable)))
+
+
+class cut(object):
+    def __init__(self, iterable):
+        self.iterable = iterable
+
+    def __getitem__(self, key):
+        if not is_iterable(key):
+            key = key,
+        return _cut(self.iterable, *key)
 
 
 def dmp(thing):
