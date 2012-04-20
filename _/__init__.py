e52440a9eb182dfae1d6805575b37faff515c2fb
Florian Mounier <florian.mounier@kozea.fr>
2012-04-20 18:24:22 +0200

    Fun with ellipsis

--- _/__init__.py
+++ _/__init__.py
@@ -1,17 +1,42 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint
 
+is_string = lambda s: (
+    isinstance(s, bytes) or
+    isinstance(s, str))
 
-def cut(iterable, index=0):
+is_iterable = lambda i: (
+    hasattr(i, '__iter__') and not
+    isinstance(i, dict) and not
+    is_string(i))
+
+
+def flatten(iterable):
+    return [
+        item
+        for subiterable in iterable
+        for item in flatten(subiterable)
+    ] if is_iterable(iterable) else [iterable]
+
+
+def cut(iterable, *args):
     """Cut a list by index or arg"""
     void = object()
+    if not args:
+        args = 0,
+
+    if len(args) > 1:
+        iterable = cut(iterable, *args[:-1])
+    index = args[-1]
+    if index == ...:
+        return flatten(iterable)
 
     def cut_item(item):
-        if isinstance(item, list) and isinstance(index, int):
-            return item[index] if len(item) > index else void
+        if isinstance(item, list) and index.isdigit():
+            return item[int(index)] if len(item) > int(index) else void
         if isinstance(item, dict):
-            return item.get(index, getattr(item, str(index), void))
-        return getattr(item, str(index), void)
+            return item.get(index, getattr(item, index, void))
+        return getattr(item, index, void)
     return list(filter(lambda x: x is not void, map(cut_item, iterable)))
 
 
--- test/test__init__.py
+++ test/test__init__.py
@@ -1,53 +1,76 @@
-from _ import cut
+from _ import cut, flatten
 
 
-def test_cut_list_of_dict():
-    list_of_dict = [
-        {'a': 'a', 'b': 2, 0: 0},
-        {'a': None, 'c': 4},
-        {'z': 5, 'a': 0, 'b': 3, 5: 'foo'}
+# def test_cut_list_of_dict():
+#     list_of_dict = [
+#         {'a': 'a', 'b': 2, '0': 0},
+#         {'a': None, 'c': 4},
+#         {'z': 5, 'a': 0, 'b': 3, '5': 'foo'}
+#     ]
+#     assert cut(list_of_dict, 'a') == ['a', None, 0]
+#     assert cut(list_of_dict, 'b') == [2, 3]
+#     assert cut(list_of_dict, 'j') == []
+#     assert cut(list_of_dict, 5) == ['foo']
+#     assert cut(list_of_dict, 2) == []
+#     assert cut(list_of_dict) == [0]
+
+
+# def test_cut_list_of_list():
+#     list_of_list = [
+#         list(range(4)),
+#         list(reversed(range(4))),
+#         list(range(12, 37, 3))
+#     ]
+#     assert cut(list_of_list, 3) == [3, 0, 21]
+#     assert cut(list_of_list, 1) == [1, 2, 15]
+#     assert cut(list_of_list, 5) == [27]
+#     assert cut(list_of_list, 50) == []
+#     assert cut(list_of_list) == [0, 3, 12]
+
+
+# def test_cut_list_of_obj():
+
+#     class cls(object):
+#         def __init__(self, **kwargs):
+#             self.__dict__.update(kwargs)
+
+#     list_of_obj = [
+#         cls(at1=1, at2=23, at3=None, at4='foo'),
+#         cls(at1=2, at2='bar', at4=cls()),
+#         cls(at1=[2., 4], at2=23, at3=None),
+#         cls(at2={})
+#     ]
+
+#     assert cut(list_of_obj, 'at1') == [1, 2, [2., 4]]
+#     assert cut(list_of_obj, 'at2') == [23, 'bar', 23, {}]
+#     assert cut(list_of_obj, 'at3') == [None, None]
+#     assert cut(list_of_obj, 'at5') == []
+
+def test_flatten():
+    assert flatten([1, 2, 3]) == [1, 2, 3]
+    assert flatten([[1], [2, 3]]) == [1, 2, 3]
+    assert flatten([[[1, 2], [3]], [4, [5]]]) == [1, 2, 3, 4, 5]
+    assert flatten([1]) == [1]
+    assert flatten(['ab']) == ['ab']
+
+
+def test_complex_cuts_list_of_dicts_of_dicts():
+    list_complex = [
+        {'A': {'b': 1, 'c': 2},
+         'B': {'b': 12, 'd': 21}},
+        {'C': {'a': 0, 'c': 2},
+         'B': {'b': 'u', 'd': None, 'a': {'p': 2, 'T': 12}}},
+        {'B': {'a': {'T': 4, 'a': 3}, 'b': 17},
+         'A': {'p': 'a', 'e': '_', 'b': ''}},
+        {'D': {'c': '0', 'z': 'w'},
+         'C': {'c': 'u', 'p': 'u'}}
     ]
-    assert cut(list_of_dict, 'a') == ['a', None, 0]
-    assert cut(list_of_dict, 'b') == [2, 3]
-    assert cut(list_of_dict, 'j') == []
-    assert cut(list_of_dict, 5) == ['foo']
-    assert cut(list_of_dict, 2) == []
-    assert cut(list_of_dict) == [0]
+    assert cut(list_complex, 'A', 'b') == [1, '']
+    assert cut(list_complex, 'B', 'b') == [12, 'u', 17]
+    assert cut(list_complex, 'B', 'a', 'T') == [12, 4]
 
 
-def test_cut_list_of_list():
-    list_of_list = [
-        list(range(4)),
-        list(reversed(range(4))),
-        list(range(12, 37, 3))
-    ]
-    assert cut(list_of_list, 3) == [3, 0, 21]
-    assert cut(list_of_list, 1) == [1, 2, 15]
-    assert cut(list_of_list, 5) == [27]
-    assert cut(list_of_list, 50) == []
-    assert cut(list_of_list) == [0, 3, 12]
-
-
-def test_cut_list_of_obj():
-
-    class cls(object):
-        def __init__(self, **kwargs):
-            self.__dict__.update(kwargs)
-
-    list_of_obj = [
-        cls(at1=1, at2=23, at3=None, at4='foo'),
-        cls(at1=2, at2='bar', at4=cls()),
-        cls(at1=[2., 4], at2=23, at3=None),
-        cls(at2={})
-    ]
-
-    assert cut(list_of_obj, 'at1') == [1, 2, [2., 4]]
-    assert cut(list_of_obj, 'at2') == [23, 'bar', 23, {}]
-    assert cut(list_of_obj, 'at3') == [None, None]
-    assert cut(list_of_obj, 'at5') == []
-
-
-def fail_complex_cuts():
+def test_complex_cuts_list_of_dicts_of_list_of_dicts():
     list_complex = [
         {'A': [
             {'b': 1, 'c': 2},
@@ -66,6 +89,6 @@ def fail_complex_cuts():
             {'c': 'u', 'p': 'u'}],
          }
     ]
-    assert cut(list_complex, 'A.b') == [1, 12]
-    assert cut(list_complex, 'B.b') == ['u', '', 17]
-    assert cut(list_complex, 'B.a.T') == [12, 4]
+    assert cut(list_complex, 'A', ..., 'b') == [1, 12]
+    assert cut(list_complex, 'B', ..., 'b') == ['u', '', 17]
+    assert cut(list_complex, 'B', ..., 'a', 'T') == [12, 4]
